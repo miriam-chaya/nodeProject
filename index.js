b17740c1d0aafc53decb222e7a428a49d0dfbc65
miriam-chaya <ch63893@gmail.com>
2024-07-24 22:36:24 +0300

    start

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import OpenAI from 'openai';
+import dotenv from 'dotenv';
 
-
-
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const app = express();
-const dotenv=dotenv.config();
-app.set('view engine', 'ejs');
+dotenv.config();
 app.use(bodyParser.json());
 app.use(cors());
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
-app.get("/", TasksController.getList);
-app.get("/:id", TasksController.getById);
-app.post("/", TasksController.add);
-app.put("/:id", TasksController.update);
-app.delete("/:id", TasksController.delete);
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
+
+app.get("/", TasksController.get);
+app.post("/prompts", TasksController.prompts);
+
+const port =  process.env.PORT;
   
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on http://localhost:${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${process.env.port}`)
 })
 
